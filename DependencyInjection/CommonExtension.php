b95936f376e7b8d62e884037503862e956382ed5
polonair <fixnim@gmail.com>
2016-08-06 12:18:18 +0400

    issue #2 closed
    Сконфигурированы необходимы сервисы.
    
    Изменено:
      - DependencyInjection/CommonExtension.php - переименован файл services.xml
        -> common.xml;
      - Resources/config/services.xml -> common.xml - сконфигурированы сервисы
        user, settings и фильтр schedule для twig.

--- DependencyInjection/CommonExtension.php
+++ DependencyInjection/CommonExtension.php
@@ -23,6 +23,6 @@ class CommonExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader->load('common.xml');
     }
 }
